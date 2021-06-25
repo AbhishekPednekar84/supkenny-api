c9f921e0a19bb07428b25a44206279858d314d82
Abhishek Pednekar <abhi_ap@hotmail.com>
2021-06-25 23:52:23 +0530

    Changes to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ if [ $TRAVIS_BRANCH == 'main' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  rsync -a --exclude={"tests","deploy.sh","travis_rsa.enc","alembic","alembic.ini","isort.cfg"} * travis@68.183.91.18:/supkenny-api
+  rsync -a --exclude={"tests","deploy.sh","travis_rsa.enc","alembic","alembic.ini","isort.cfg"} * travis@68.183.91.18:/home/abhiap/supkenny-api
   echo "Deployed successfully!"
 else
   echo "Not deploying, since the branch isn't main."
