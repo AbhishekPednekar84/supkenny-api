5f825e9cb045e40b76f59ad209ea113a78ae1270
Abhishek Pednekar <abhi_ap@hotmail.com>
2021-06-25 23:38:58 +0530

    Setup for travis

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+set -xe
+
+if [ $TRAVIS_BRANCH == 'main' ] ; then
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/id_rsa
+
+  rsync -a --exclude={"tests","deploy.sh","travis_rsa.enc","alembic","alembic.ini","isort.cfg"} * travis@68.183.91.18:/supkenny-api
+  echo "Deployed successfully!"
+else
+  echo "Not deploying, since the branch isn't main."
+fi
